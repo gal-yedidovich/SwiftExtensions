4295fb402977ba13d554bc51bf937e95a5cac7ea
galy <galy@ctera.com>
2020-06-15 10:12:26 +0300

    CHANGE  - indentation changes  - made JSON methods public

--- Package.swift
+++ Package.swift
@@ -4,26 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "Extensions",
+	name: "Extensions",
 	platforms: [.iOS(.v13)],
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Extensions",
-            targets: ["Extensions"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Extensions",
-            dependencies: []),
-        .testTarget(
-            name: "ExtensionsTests",
-            dependencies: ["Extensions"]),
-    ]
+	products: [
+		// Products define the executables and libraries produced by a package, and make them visible to other packages.
+		.library(
+			name: "Extensions",
+			targets: ["Extensions"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
+		.target(
+			name: "Extensions",
+			dependencies: []),
+		.testTarget(
+			name: "ExtensionsTests",
+			dependencies: ["Extensions"]),
+	]
 )
--- Sources/Extensions/EncodingExt.swift
+++ Sources/Extensions/EncodingExt.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Encodable {
 	/// encode in JSON encoding
 	/// - Returns: JSON represention of data
-	func json() -> Data {
+	public func json() -> Data {
 		try! JSONEncoder().encode(self)
 	}
 }
@@ -19,7 +19,7 @@ extension Decodable {
 	/// Decode data in JSON decoding
 	/// - Parameter json: JSON encoded data
 	/// - Returns: Generic Decodable value representing the JSON
-	static func from <T: Decodable> (json: Data) -> T {
+	public static func from <T: Decodable> (json: Data) -> T {
 		try! JSONDecoder().decode(T.self, from: json)
 	}
 }
