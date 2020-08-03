766d2179d5992b516af749299b52d57558a641bd
Gal <galy@ctera.com>
2020-08-03 12:23:16 +0300

    CHANGE exposing `keyAccessability` value to the caller

--- Sources/StorageExtensions/Encryptor.swift
+++ Sources/StorageExtensions/Encryptor.swift
@@ -33,6 +33,11 @@ public class Encryptor {
 		kSecAttrAccount: "SwiftStorage", //login
 	]
 	
+	/// This value control when the encryption key is accessible
+	///
+	/// This value is used when storing a newly created key in the Keychain. Since the key is resued this value is used once, as long as the key is exists in the Keychain.
+	public static var keyAccessability = kSecAttrAccessibleAfterFirstUnlock
+	
 	/// Encrypt data with CGM encryption, and returns the encrypted data in result
 	/// - Parameter data: the data to encrypt
 	/// - Returns: encrypted data
@@ -132,7 +137,7 @@ public class Encryptor {
 	private static func storeNewKey() -> SymmetricKey {
 		let key = SymmetricKey(size: .bits256) //create new key
 		var query = keyChainQuery
-		query[kSecAttrAccessible] = kSecAttrAccessibleAfterFirstUnlock
+		query[kSecAttrAccessible] = keyAccessability
 		query[kSecValueData] = key.dataRepresentation //request to get the result (key) as data
 		
 		let status = SecItemAdd(query as CFDictionary, nil)
