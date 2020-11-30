e296c065e1730f3ed3e8135a6320b489fefa8388
Gal <galy@ctera.com>
2020-11-30 07:49:05 +0200

    CHANGE  - PrefsValue:   + added @State value to update changes   + removed Optional to allow binding in a projectedValue

--- Sources/StorageExtensions/Prefs.swift
+++ Sources/StorageExtensions/Prefs.swift
@@ -208,33 +208,32 @@ public struct PrefKey {
 
 import SwiftUI
 /// A linked value in the Prefs, allowing to read & write.
+/// This wrapper will call update on UI when its value changes
 @propertyWrapper
 public struct PrefsValue<Value>: DynamicProperty where Value: Codable {
+	@State private var value: Value
 	private let key: PrefKey
 	private let prefs: Prefs
 	
-	public init(wrappedValue defValue: Value? = nil, key: PrefKey, prefs: Prefs = .standard) {
+	public init(wrappedValue defValue: Value, key: PrefKey, prefs: Prefs = .standard) {
 		self.key = key
 		self.prefs = prefs
-		if !prefs.contains(key: key), let defValue = defValue {
-			self.wrappedValue = defValue
+		_value = State(initialValue: prefs.codable(key: key) ?? defValue)
+	}
+	
+	public var wrappedValue: Value {
+		get { value }
+		nonmutating set {
+			prefs.edit().put(key: key, newValue).commit()
+			
+			value = newValue
 		}
 	}
 	
-	public var wrappedValue: Value? {
-		get { prefs.codable(key: key) }
-		set {
-			let editor = prefs.edit()
-			
-			if let value = newValue {
-				_ = editor.put(key: key, value)
-			} else {
-				_ = editor.remove(key: key)
-			}
-
-			editor.commit()
-			
-			update()
-		}
+	public var projectedValue: Binding<Value> {
+		Binding (
+			get: { wrappedValue },
+			set: { wrappedValue = $0 }
+		)
 	}
 }
