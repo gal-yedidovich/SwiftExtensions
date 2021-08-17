0d9b76277da9a3d205f77348fd953f24d7685123
gal.yedidovich <gal.yedidovich@ironsrc.com>
2021-08-17 15:44:22 +0300

    ADDED CryptoExtensions dependency REMOVED crypto module & tests CHANGED parameter name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,9 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
+let dep = Target.Dependency.product(name: "SimpleEncryptor", package: "CryptoExtensions")
 
 let package = Package(
     name: "SwiftExtensions",
@@ -10,20 +12,21 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(name: "BasicExtensions", targets: ["BasicExtensions"]),
 		.library(name: "StorageExtensions", targets: ["StorageExtensions"]),
-		.library(name: "CryptoExtensions", targets: ["CryptoExtensions"]),
+//		.library(name: "CryptoExtensions", targets: ["CryptoExtensions"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+		.package(name: "CryptoExtensions", url: "https://github.com/gal-yedidovich/CryptoExtensions", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(name: "BasicExtensions"),
-		.target(name: "CryptoExtensions", dependencies: ["BasicExtensions"]),
-        .target(name: "StorageExtensions", dependencies: ["BasicExtensions", "CryptoExtensions"]),
+//		.target(name: "CryptoExtensions", dependencies: ["BasicExtensions"]),
+		.target(name: "StorageExtensions", dependencies: ["BasicExtensions", dep]),
         .testTarget(name: "BasicExtensionsTests", dependencies: ["BasicExtensions"]),
-		.testTarget(name: "CryptoTests", dependencies: ["CryptoExtensions"]),
+//		.testTarget(name: "CryptoTests", dependencies: ["CryptoExtensions"]),
         .testTarget(name: "StorageTests", dependencies: ["BasicExtensions", "StorageExtensions"]),
     ]
 )
